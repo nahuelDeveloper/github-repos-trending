2c99fbc45175e57a667179b8a0bf73fad0a8d31a
Nahuel Jose Roldan <>
2024-04-21 08:39:30 -0300

    Create Xcode project

---
+++ Github Repos Trending/Github_Repos_TrendingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Github_Repos_TrendingApp.swift
+//  Github Repos Trending
+//
+//  Created by Nahuel Jose Roldan on 21/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct Github_Repos_TrendingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Github Repos TrendingUITests/Github_Repos_TrendingUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Github_Repos_TrendingUITestsLaunchTests.swift
+//  Github Repos TrendingUITests
+//
+//  Created by Nahuel Jose Roldan on 21/04/2024.
+//
+
+import XCTest
+
+final class Github_Repos_TrendingUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
