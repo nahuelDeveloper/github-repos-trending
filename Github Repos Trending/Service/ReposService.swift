9a53df7c076f0176e5b22b566f8ecf8459908a08
Nahuel Jose Roldan <>
2024-04-22 19:23:03 -0300

    Add logic in RepoService to retrieve repos from local storage or from the API

--- Github Repos Trending/Service/ReposService.swift
+++ Github Repos Trending/Service/ReposService.swift
@@ -9,7 +9,31 @@ import Foundation
 import Combine
 
 class ReposService: ReposServicing {
+    private var cancellables = Set<AnyCancellable>()
+    
     func fetchRepos() -> AnyPublisher<[Repo], Error> {
+        return fetchReposFromLocalStorage()
+            .flatMap { localRepos -> AnyPublisher<[Repo], Error> in
+                if localRepos.isEmpty {
+                    return self.fetchReposFromAPI()
+                        .handleEvents(receiveOutput: { fetchedRepos in
+                            StorageManager.shared.saveRepos(fetchedRepos)
+                        })
+                        .eraseToAnyPublisher()
+                } else {
+                    return Just(localRepos)
+                        .setFailureType(to: Error.self)
+                        .eraseToAnyPublisher()
+                }
+            }
+            .eraseToAnyPublisher()
+    }
+        
+    private func fetchReposFromLocalStorage() -> AnyPublisher<[Repo], Error> {
+        return StorageManager.shared.loadRepos()
+    }
+    
+    private func fetchReposFromAPI() -> AnyPublisher<[Repo], Error> {
         let url = URL(string: "https://api.github.com/search/repositories?q=language=+sort:stars")!
         return NetworkManager.fetchURL(url)
     }
